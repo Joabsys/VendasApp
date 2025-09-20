b47e8fbdfe1d1ce76e8d8bb2e090f5a310f5aa49
Miguel Elias Dias <37157587+MiguelElias@users.noreply.github.com>
2025-09-20 09:40:09 -0300

    Mudança do caminho do projeto

---
+++ VendasApp.Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendasApp.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataUltimoLogin { get; set; }
+    }
+}
